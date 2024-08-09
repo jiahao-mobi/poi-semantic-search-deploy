cbbd2d3b238b266ca05861f0f0d56eed278332c9
jiahao-mobi <jiahao@takemobi.com>
2024-08-08 20:33:30 -0400

    add tui poi search

--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ from model import *
 
 app = Flask(__name__)
 
-model_and_tokenizer = model_fn(model_dir="sentence-transformers/all-MiniLM-L12-v2")
+
+MODEL_AND_TOKENIZER = model_fn(model_dir="sentence-transformers/all-MiniLM-L12-v2")
+
 
 @app.route('/user_query_tag_search', methods=['POST'])
 def user_query_tag_search():
@@ -17,19 +19,37 @@ def user_query_tag_search():
     if 'topK' not in data:
         return jsonify({"error": "Missing 'topK' in request"})
     
-    # Tokenize sentences
     query = data.pop("inputs")
     query_index = data.pop("index")
     query_topK = int(data.pop("topK"))
     
-    if query_index not in LOCAL_INDEX:
-        return jsonify({"error": f"Expect index be in {LOCAL_INDEX}. But get {query_index}"})
+    if query_index not in LOCAL_TAG_INDEX:
+        return jsonify({"error": f"Expect index be in {LOCAL_TAG_INDEX}. But get {query_index}"})
     
-    results = predict_fn(query, query_index, query_topK, model_and_tokenizer)
+    results = predict_topk_tags(query, query_index, query_topK, MODEL_AND_TOKENIZER)
     
     return jsonify(results)
 
 
+@app.route('/user_query_tui_poi_search', methods=['POST'])
+def user_query_tui_poi_search():
+    data = request.json
+    
+    if 'inputs' not in data:
+        return jsonify({"error": "Missing 'inputs' in request"})
+    if 'topK' not in data:
+        return jsonify({"error": "Missing 'topK' in request"})
+    
+    query_columns = None
+    if 'columns' in data:
+        query_columns =  data.pop("columns")
+    
+    query = data.pop("inputs")
+    query_topK = int(data.pop("topK"))
+    
+    results = predict_topk_pois(query, query_topK, MODEL_AND_TOKENIZER, query_columns)
+    
+    return jsonify(results)
 
 
 if __name__ == '__main__':
--- src/model.py
+++ src/model.py
@@ -3,48 +3,14 @@ import torch
 import torch.nn.functional as F
 import pickle
 import os
+import pandas as pd
+import numpy as np
 
 
-# ----------------------------- #
-#      Index For TopK Search    #
-# ----------------------------- #
-LOCAL_INDEX_FOLDER = "local_tag_emb_index"
-
-LOCAL_INDEX = [
-    "dining-tag-vector", 
-    "experience-tag-vector", 
-    "accommodation-tag-vector",
-    "accommodation-brand-tag-vector",
-    "tui-accommodation-tag-vector",
-    "ids-accommodation-brand-tag-vector",
-]
-
-
-def load_index(index):
-    with open(os.path.join(LOCAL_INDEX_FOLDER, f"{index}.pkl"), 'rb') as f:
-        data = pickle.load(f)
-    return { "tags": data["tags"], "embeddings": data["embeddings"] }
-
-
-INDEX2DATA = { idx:load_index(idx) for idx in LOCAL_INDEX }
-
-def retrieve_topk_tags(query_emb, query_index, topk=5):
-    assert query_emb.shape  == (1,384)
     
-    index_data = INDEX2DATA[query_index]
-    
-    tag_list, tag_embeddings = index_data['tags'], index_data['embeddings']
-    
-    similarities = F.cosine_similarity(query_emb, tag_embeddings, dim=1)
-    
-    # Sort indices based on similarity in descending order
-    sorted_indices = torch.argsort(similarities, descending=True).tolist()
-    
-    topk_tags = [tag_list[idx] for idx in sorted_indices[:topk]]
-    
-    return topk_tags
-    
-    
+# -------------------- #
+#    Model Function    #
+# -------------------- #
 # Helper: Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0] #First element of model_output contains all token embeddings
@@ -58,18 +24,83 @@ def model_fn(model_dir):
     return model, tokenizer
 
 
-def predict_fn(query, query_index, query_topK, model_and_tokenizer):
-    # destruct model and tokenizer
-    model, tokenizer = model_and_tokenizer
-    encoded_input = tokenizer(query, padding=True, truncation=True, return_tensors='pt')
-    # Compute token embeddings
+def model_encode(model, tokenizer, text):
+    encoded_input = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
     with torch.no_grad():
         model_output = model(**encoded_input)
     # Perform pooling
-    query_emb = mean_pooling(model_output, encoded_input['attention_mask'])
-    # Normalize embeddings
-    query_emb = F.normalize(query_emb, p=2, dim=1)
-    
+    text_emb = mean_pooling(model_output, encoded_input['attention_mask'])
+    text_emb = F.normalize(text_emb, p=2, dim=1)
+    return text_emb
+
+
+# ----------------------- #
+#      Tag TopK Search    #
+# ----------------------- #
+LOCAL_TAG_INDEX_FOLDER = "local_tag_emb_index"
+
+LOCAL_TAG_INDEX = [
+    "dining-tag-vector", 
+    "experience-tag-vector", 
+    "accommodation-tag-vector",
+    "accommodation-brand-tag-vector",
+    "tui-accommodation-tag-vector",
+    "ids-accommodation-brand-tag-vector",
+]
+
+
+def load_index(index):
+    with open(os.path.join(LOCAL_TAG_INDEX_FOLDER, f"{index}.pkl"), 'rb') as f:
+        data = pickle.load(f)
+    return { "tags": data["tags"], "embeddings": data["embeddings"] }
+
+
+INDEX2DATA = { idx:load_index(idx) for idx in LOCAL_TAG_INDEX }
+
+
+def retrieve_topk_tags(query_emb, query_index, topk=5):
+    assert query_emb.shape  == (1,384)
+    index_data = INDEX2DATA[query_index]
+    tag_list, tag_embeddings = index_data['tags'], index_data['embeddings']
+    similarities = F.cosine_similarity(query_emb, tag_embeddings, dim=1)
+    # Sort indices based on similarity in descending order
+    sorted_indices = torch.argsort(similarities, descending=True).tolist()
+    topk_tags = [tag_list[idx] for idx in sorted_indices[:topk]]
+    return topk_tags
+
+
+def predict_topk_tags(query, query_index, query_topK, model_and_tokenizer):
+    # destruct model and tokenizer
+    model, tokenizer = model_and_tokenizer
+    query_emb = model_encode(model, tokenizer, query)
     topK_tags = retrieve_topk_tags(query_emb, query_index, query_topK)
     # return dictonary, which will be json serializable
-    return {"topK_tags":topK_tags}
+    return {"topK_tags": topK_tags}
+
+
+# --------------------------- #
+#      Tui POI TopK Search    #
+# --------------------------- #
+TUI_POI_DF = pd.read_parquet(f"./poi_desc_emb/tui-musement-backup-accommodation-embedded.parquet")
+TUI_POI_EMB = torch.Tensor(np.vstack(TUI_POI_DF['all-MiniLM-L12-v2']))
+
+def retrieve_topk_pois(query_emb, topk=5):
+    assert query_emb.shape == (1,384), f'expected to get query_emb shape (1,384), but get {query_emb.shape}'
+    similarities = F.cosine_similarity(query_emb, TUI_POI_EMB, dim=1)
+    # Sort indices based on similarity in descending order
+    sorted_indices = torch.argsort(similarities, descending=True).tolist()
+    return TUI_POI_DF.iloc[sorted_indices[:topk]]
+
+
+def predict_topk_pois(query, query_topK, model_and_tokenizer, query_columns=None):
+    # destruct model and tokenizer
+    model, tokenizer = model_and_tokenizer
+    query_emb = model_encode(model, tokenizer, query)
+    topK_pois = retrieve_topk_pois(query_emb, query_topK)
+    
+    if query_columns:
+        result = topK_pois[query_columns].to_dict(orient='records')
+    else:
+        result = topK_pois.to_dict(orient='records')
+
+    return {"topK_pois": result}
